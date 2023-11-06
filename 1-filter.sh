852bba6f8c5220c8e40317ceb64f255fb47b52c1
paojn <paojn@mail.uc.edu>
2023-11-05 19:06:14 -0500

    filter.sh successfully passed

--- 1-filter.sh
+++ 1-filter.sh
@@ -4,4 +4,4 @@
 input_file="email_list.txt"
 
 # Use grep to filter and display email addresses ending with "@gmail.com" (you must use a regular expression for this)
-
+grep -E '\b[A-Za-z0-9._%+-]+@gmail\.com\b' "$input_file"
